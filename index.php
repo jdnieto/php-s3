5b50cbf4b7f16804dad7cadf52f7e3a36d580054
José David Martín Nieto <j.david.nieto@gmail.com>
2017-03-23 00:03:11 +0100

    fixed endpoint

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory(array('key'=>getenv('AWS_ACCESS_KEY_ID'),'secret'=>getenv('AWS_SECRET_ACCESS_KEY')));
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var found in env!');
-$endpoint = getenv ('S3_ENDPOINT')?: die('No "S3_ENDPOINT" config var found in env!');
+$s3_endpoint = getenv('S3_ENDPOINT')?: die('No "S3_ENDPOINT" config var found in env!');
+$access_key = getenv('S3_KEY')?: die('No "S3_KEY" config var found in env!');
+$access_secret = getenv('S3_SECRET')?: die('No "S3_SECRET" config var found in env!');
+$s3 = Aws\S3\S3Client::factory(array('key'=>$access_key,'secret'=>$access_secret,'base_url'=>$s3_endpoint));
 ?>
 <html>
     <head><meta charset="UTF-8"></head>
@@ -14,7 +16,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
     // FIXME: add more validation, e.g. using ext/fileinfo
     try {
         // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-        $s3->setEndpoint($endpoint);
         $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 ?>
         <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
