bb8920d4393ec447867936d78e66417b5f28c1d9
José David Martín Nieto <j.david.nieto@gmail.com>
2017-03-22 15:06:11 +0100

    fixed connection

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
+$s3 = Aws\S3\S3Client::factory(array('key'=>getenv('AWS_ACCESS_KEY_ID'),'secret'=>getenv('AWS_SECRET_ACCESS_KEY')));
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var found in env!');
 $endpoint = getenv ('S3_ENDPOINT')?: die('No "S3_ENDPOINT" config var found in env!');
 ?>
